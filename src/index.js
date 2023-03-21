efc2ff3570370d9ef3095ab5760ff0ef4e56778f
Eric Krukowski <erickruk@amazon.com>
2023-03-21 04:55:38 -0400

    reformatted single line functions, added comments, turned hide and seek into a ternary statement

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  const addBtn = document.querySelector("#new-toy-btn")
+  const toyFormContainer = document.querySelector(".container")
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    if (toyFormContainer.style.display === "block") {
-      toyFormContainer.style.display = "none";
-    } else {
-      toyFormContainer.style.display = "block";
-    }
-  });
-
-  //Fetch toys
-  fetch(`http://localhost:3000/toys`)
-  .then(resp => {
-    //turns JSON to object  
-    return resp.json();
+    // hide & seek with the form ternary style
+    toyFormContainer.style.display === "block" ? toyFormContainer.style.display = "none" : toyFormContainer.style.display = "block" 
   })
+
+  //Fetch toys for initialization
+  fetch(`http://localhost:3000/toys`)
+  .then(resp => resp.json())
   .then(deck => {
-  //Add toy to card
-    for(toy of deck){
-     addToy(toy)
-    }
+  //Sends each toy found in the database to add to the collection
+    for(toy of deck){addToy(toy)}
   })
   //Adds a toy to the collection
   function addToy(toy){
+    //create the HTML elements and attributes
     const newToy = document.createElement('div')
     newToy.classList.add("card")
     const cardH2 = document.createElement('h2')
@@ -39,21 +31,19 @@ document.addEventListener("DOMContentLoaded", () => {
     cardButton.classList.add("like-btn")
     cardButton.textContent = "Like"
     cardButton.addEventListener('click', addLike)
-    newToy.appendChild(cardH2)
-    newToy.appendChild(cardImg)
-    newToy.appendChild(cardLikes)
-    newToy.appendChild(cardButton)
+    newToy.append(cardH2, cardImg, cardLikes, cardButton)
+    //adds card to the toy-collection
     document.querySelector('#toy-collection').appendChild(newToy)
   }
   //Increase likes
   function addLike(){
     const cardInfo = {
       name: this.parentNode.querySelector('h2').textContent,
-      likes: parseInt(this.parentNode.querySelector('p').textContent, 10)+1,
+      likes: parseInt(this.parentNode.querySelector('p').textContent, 10)+1, //Raises the value of likes by 1 here
       image: this.parentNode.querySelector('img').src,
       id: this.id
     }
-    const postData = {
+    const patchData = {
       method: "PATCH",
       headers: {
         "Content-Type" : "application/json",
@@ -61,15 +51,13 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       body: JSON.stringify(cardInfo)
     }
-    fetch(`http://localhost:3000/toys/${this.id}`, postData)
+    //Patches the object on the server
+    fetch(`http://localhost:3000/toys/${this.id}`, patchData)
     .then(resp => resp.json())
-    .then(data => {
-      this.parentNode.querySelector('p').textContent = `${data.likes} likes`
-    })
-    .catch(e=>console.log(e))
+    .then(data => this.parentNode.querySelector('p').textContent = `${data.likes} likes`) //updates the webpage when patch is successful
   }
 
-  //adds a new toy through the add toy feature
+  //adds a new toy through the add toy form
   const addToyForm = document.querySelector('form')
   addToyForm.addEventListener('submit', createToy=>{
     createToy.preventDefault()
@@ -88,8 +76,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     fetch(`http://localhost:3000/toys/`, postData)
     .then(resp => resp.json())
-    .then(toyObj => {
-      addToy(toyObj)
-    })
+    .then(toyObj => addToy(toyObj)) //Adds the new toy to the html doc when post is successful
   })
-});
+})
